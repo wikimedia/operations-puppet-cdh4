306e3a393f43cd0b205b3729c13a623d52b87d1c
Andrew Otto <acotto@gmail.com>
2012-10-19 12:31:25 -0400

    Fixing oozie database configs

--- manifests/oozie/server.pp
+++ manifests/oozie/server.pp
@@ -3,6 +3,7 @@
 class cdh4::oozie::server(
 	$jdbc_driver   = 'org.apache.derby.jdbc.EmbeddedDriver',
 	$jdbc_url      = 'jdbc:derby:${oozie.data.dir}/${oozie.db.schema.name}-db;create=true',
+	$jdbc_database = 'oozie',
 	$jdbc_username = "sa",
 	$jdbc_password = " ")
 {
@@ -11,6 +12,7 @@ class cdh4::oozie::server(
 	class { "cdh4::oozie::server::config": 
 		jdbc_driver   => $jdbc_driver,
 		jdbc_url      => $jdbc_url,
+		jdbc_database => $jdbc_database,
 		jdbc_username => $jdbc_username,
 		jdbc_password => $jdbc_password,
 		require       => Class["cdh4::oozie::server::install"],
--- manifests/oozie/server/config.pp
+++ manifests/oozie/server/config.pp
@@ -1,13 +1,13 @@
 # == Class cdh4::oozie::server::install
 #
-class cdh4::oozie::server::config($jdbc_driver, $jdbc_url, $jdbc_username, $jdbc_password)
+class cdh4::oozie::server::config($jdbc_driver, $jdbc_url, $jdbc_database, $jdbc_username, $jdbc_password)
 {
-	file { "/etc/oozie/oozie-env.sh":
+	file { "/etc/oozie/conf/oozie-env.sh":
 		content => template("cdh4/oozie/oozie-env.sh.erb"),
 		require => Package["oozie"],
 	}
 
-	file { "/etc/oozie/oozie-site.xml":
+	file { "/etc/oozie/conf/oozie-site.xml":
 		content => template("cdh4/oozie/oozie-site.xml.erb"),
 		require => Package["oozie"],
 	}
