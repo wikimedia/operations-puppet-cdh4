81e89bc69ce883fcd8649cf970c7ae3bb50a6afc
Andrew Otto <acotto@gmail.com>
2012-10-22 17:40:06 -0400

    Configuring hive-site.xml.erb with Zookeeper hosts

--- manifests/hive/server/config.pp
+++ manifests/hive/server/config.pp
@@ -2,6 +2,10 @@
 #
 class cdh4::hive::server::config($jdbc_driver, $jdbc_url, $jdbc_username, $jdbc_password)
 {
+	# bring this into a local variable so we can
+	# tests for its definition in the hive-site.xml.erb template.
+	$zookeeper_hosts = $cdh4::zookeeper::config::zookeeper_hosts
+
 	file { "/etc/hive/conf/hive-site.xml":
 		content => template("cdh4/hive/hive-site.xml.erb"),
 		require => Package["hive"],
