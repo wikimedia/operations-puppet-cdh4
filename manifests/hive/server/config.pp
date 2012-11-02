c6301a823656c83e0cab9d6515642eaf30be5ca4
Andrew Otto <acotto@gmail.com>
2012-11-02 16:39:41 -0400

    Adding more parameters for hive-site.xml configuration

--- manifests/hive/server/config.pp
+++ manifests/hive/server/config.pp
@@ -1,6 +1,18 @@
 # == Class cdh4::hive::server::config
 #
-class cdh4::hive::server::config($jdbc_driver, $jdbc_url, $jdbc_username, $jdbc_password)
+class cdh4::hive::server::config(
+	$jdbc_driver,
+	$jdbc_url,
+	$jdbc_username,
+	$jdbc_password,
+	$exec_parallel_thread_number = 8,  # set this to 0 to disable hive.exec.parallel
+	$optimize_skewjoin           = false,
+	$skewjoin_key                = 10000,
+	$skewjoin_mapjoin_map_tasks  = 10000,
+	$stats_enabled               = false,
+	$stats_dbclass               = "jdbc:derby",
+	$stats_jdbcdriver            = "org.apache.derby.jdbc.EmbeddedDriver",
+	$stats_dbconnectionstring    = "jdbc:derby:;databaseName=TempStatsStore;create=true")
 {
 	# bring this into a local variable so we can
 	# tests for its definition in the hive-site.xml.erb template.
