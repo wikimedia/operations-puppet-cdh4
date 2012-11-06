ca60763414533e54f6d7d06687830b33d83db083
Andrew Otto <acotto@gmail.com>
2012-11-06 15:50:02 -0500

    Puppetizing Oozie/Catalina workdirs

--- manifests/oozie/server.pp
+++ manifests/oozie/server.pp
@@ -18,7 +18,29 @@ class cdh4::oozie::server(
 		require       => Class["cdh4::oozie::server::install"],
 	}
 
+	# Ensure that Catalina working directories exist.
+	# Without these, oozie will log the error:
+	# "The specified scratchDir is unusable: /usr/lib/oozie/oozie-server-0.20/work/Catalina/localhost/_"
+	file { ["/usr/lib/oozie/oozie-server/work",
+			"/usr/lib/oozie/oozie-server/work/Catalina",
+			"/usr/lib/oozie/oozie-server/work/Catalina/localhost"]:
+		ensure  => "directory",
+		owner   => "root",
+		group   => "root",
+		mode    => 0755,
+		require => Class["cdh4::oozie::server::install"],
+	}
+	file { ["/usr/lib/oozie/oozie-server/work/Catalina/localhost/_",
+			"/usr/lib/oozie/oozie-server/work/Catalina/localhost/oozie"]:
+		ensure  => "directory",
+		owner   => "oozie",
+		group   => "oozie",
+		mode    => 0755,
+		require => File["/usr/lib/oozie/oozie-server/work/Catalina/localhost"],
+	}
+
+	# ensure that oozie service is running
 	class { "cdh4::oozie::server::service":
-		require       => Class["cdh4::oozie::server::config"],
+		require => [Class["cdh4::oozie::server::config"], File["/usr/lib/oozie/oozie-server/work/Catalina/localhost/oozie"]],
 	}
 }
