5a3ff720b67c64468fb88ed1a4944ef3b244fbaf
Andrew Otto <acotto@gmail.com>
2012-12-06 12:15:21 -0500

    Puppetizing ldap for hue

--- manifests/hue.pp
+++ manifests/hue.pp
@@ -4,13 +4,29 @@
 # and ensures that hue server is running.
 # This requires that cdh4::hadoop::config is included.
 #
-class cdh4::hue($secret_key = undef) {
+class cdh4::hue(
+	$secret_key            = undef,
+	$ldap_url              = undef,
+	$ldap_cert             = undef,
+	$ldap_nt_domain        = undef,
+	$ldap_bind_dn          = undef,
+	$ldap_base_dn          = undef,
+	$ldap_bind_password    = undef,
+	$ldap_username_pattern = undef)
+{
 	include cdh4::hue::install
 
 	# config needs hue installed and hadoop config defined.
 	class { "cdh4::hue::config":
-		secret_key => $secret_key,
-		require => [Class["cdh4::hue::install"], Class["cdh4::hadoop::config"]],
+		secret_key            => $secret_key,
+		ldap_url              => $ldap_url,
+		ldap_cert             => $ldap_cert,
+		ldap_nt_domain        => $ldap_nt_domain,
+		ldap_bind_dn          => $ldap_bind_dn,
+		ldap_base_dn          => $ldap_base_dn,
+		ldap_bind_password    => $ldap_bind_password,
+		ldap_username_pattern => $ldap_username_pattern,
+		require               => [Class["cdh4::hue::install"], Class["cdh4::hadoop::config"]],
 	}
 
 	# The hue service subscribes to the hue config file.
--- manifests/hue/config.pp
+++ manifests/hue/config.pp
@@ -3,7 +3,16 @@
 # Installs hue.ini.erb.
 # requires that cdh4::hadoop::config is included.
 #
-class cdh4::hue::config($secret_key = undef) {
+class cdh4::hue::config(
+	$secret_key            = undef,
+	$ldap_url              = undef,
+	$ldap_cert             = undef,
+	$ldap_nt_domain        = undef,
+	$ldap_bind_dn          = undef,
+	$ldap_base_dn          = undef,
+	$ldap_bind_password    = undef,
+	$ldap_username_pattern = undef)
+{
 	file { "/etc/hue/hue.ini":
 		content => template("cdh4/hue/hue.ini.erb"),
 		owner   => "root",
